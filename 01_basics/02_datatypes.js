7680f85115eb02c9e370c7730ef0abd47d961727
shivafricky <125849644+shivafricky@users.noreply.github.com>
2023-09-02 04:06:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+console.log(3
+    +3);//code readability for programmer
+// console.log("shanakar")
+let name="shankar"
+let age=25
+let loggedIn=false;
+let state;
+
+console.log(name,age,loggedIn)
+console.log(typeof name)
+console.log(typeof state)
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
