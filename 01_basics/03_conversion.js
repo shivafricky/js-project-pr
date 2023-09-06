780125bf2b0236a7d3cdda8ab67bb2a8f5259291
shivafricky <125849644+shivafricky@users.noreply.github.com>
2023-09-06 04:52:55 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+let score=
+"shankar "
+//true=>1 and false=>0
+//undefined=>NaN
+//null=>0
+//"33abc"=>NaN
+//"33"=>33
+
+console.log(typeof score)
+console.log(typeof(score))
+
+
+let VinN=Number(score)
+console.log(VinN)
+console.log(typeof VinN)
+console.log(Boolean (score))
+
+
