368e6471cbbe847f316132d5f034701a3b18dcc0
shivafricky <125849644+shivafricky@users.noreply.github.com>
2023-09-07 07:02:25 +0000

    operations

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,19 @@
+let value=3
+let negValue=-value
+console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+console.log(2%2)
+let s1="shankar "
+let s2="nishu"
+let s3=s1+s2
+console.log(s3)
+console.log(1+"2")
+console.log("1"+2)
+console.log(1+2+"2")
+console.log(1+"2"+"2")
+console.log(1+"2"+2)
+console.log("1"+2+2)
+console.log(1+3*2%2)
